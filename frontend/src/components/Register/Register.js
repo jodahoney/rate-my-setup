e6a67eeb16242f2ed0747de979061f078e6534af
Joseph Dehoney <josephdehoney@gmail.com>
2021-10-16 17:45:27 -0700

    removed unnecessary commented lines from register.js

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,6 +1,3 @@
-// import { useEffect, useState } from "react"
-// import { useNavigate, Link } from "react-router-dom"
-// import apiClient from "../../services/apiClient"
 import { Link } from "react-router-dom"
 import { useRegistrationForm } from "../../hooks/useRegistrationForm"
 import "./Register.css"
