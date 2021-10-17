39fddd342f0e04488be21d7f00c24f69563f5864
Joseph Dehoney <josephdehoney@gmail.com>
2021-10-16 17:44:31 -0700

    registration custom hook added

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,75 +1,14 @@
-import { useEffect, useState } from "react"
-import { useNavigate, Link } from "react-router-dom"
-import apiClient from "../../services/apiClient"
+// import { useEffect, useState } from "react"
+// import { useNavigate, Link } from "react-router-dom"
+// import apiClient from "../../services/apiClient"
+import { Link } from "react-router-dom"
+import { useRegistrationForm } from "../../hooks/useRegistrationForm"
 import "./Register.css"
 
 export default function Register({ user, setUser }) {
-  const navigate = useNavigate()
-  const [isProcessing, setIsProcessing] = useState(false)
-  const [errors, setErrors] = useState({})
-  const [form, setForm] = useState({
-    email: "",
-    username: "",
-    password: "",
-    passwordConfirm: "",
-  })
 
-  useEffect(() => {
-    // if user is already logged in,
-    // redirect them to the home page
-    if (user?.email) {
-      navigate("/")
-    }
-  }, [user, navigate])
-
-  const handleOnInputChange = (event) => {
-    if (event.target.name === "email") {
-      if (event.target.value.indexOf("@") === -1) {
-        setErrors((e) => ({ ...e, email: "Please enter a valid email." }))
-      } else {
-        setErrors((e) => ({ ...e, email: null }))
-      }
-    }
-
-    if (event.target.name === "passwordConfirm") {
-      if (event.target.value !== form.password) {
-        setErrors((e) => ({ ...e, passwordConfirm: "Passwords do not match." }))
-      } else {
-        setErrors((e) => ({ ...e, passwordConfirm: null }))
-      }
-    }
-
-    setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
-  }
-
-  const handleOnSubmit = async () => {
-    setIsProcessing(true)
-    setErrors((e) => ({ ...e, form: null }))
-
-    if (form.passwordConfirm !== form.password) {
-      setErrors((e) => ({ ...e, passwordConfirm: "Passwords do not match." }))
-      setIsProcessing(false)
-      return
-    } else {
-      setErrors((e) => ({ ...e, passwordConfirm: null }))
-    }
-
-    const { data, error } = await apiClient.signupUser({
-      email: form.email,
-      password: form.password,
-      username: form.username,
-    })
-    if (data) {
-      setUser(data.user)
-      apiClient.setToken(data.token)
-    }
-    if (error) {
-      setErrors((e) => ({ ...e, form: error }))
-    }
-
-    setIsProcessing(false)
-  }
-
+  const { form, errors, isProcessing, handleOnInputChange, handleOnSubmit } = useRegistrationForm({ user, setUser }) 
+  
   return (
     <div className="Signup">
       <div className="card">
---
+++ frontend/src/hooks/useRegistrationForm.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import apiClient from "../services/apiClient"
+
+export const useRegistrationForm = ({ user, setUser }) => {
+
+    const navigate = useNavigate()
+    const [isProcessing, setIsProcessing] = useState(false)
+    const [errors, setErrors] = useState({})
+    const [form, setForm] = useState({
+      email: "",
+      username: "",
+      password: "",
+      passwordConfirm: "",
+    })
+  
+    useEffect(() => {
+      // if user is already logged in,
+      // redirect them to the home page
+      if (user?.email) {
+        navigate("/")
+      }
+    }, [user, navigate])
+  
+    const handleOnInputChange = (event) => {
+      if (event.target.name === "email") {
+        if (event.target.value.indexOf("@") === -1) {
+          setErrors((e) => ({ ...e, email: "Please enter a valid email." }))
+        } else {
+          setErrors((e) => ({ ...e, email: null }))
+        }
+      }
+  
+      if (event.target.name === "passwordConfirm") {
+        if (event.target.value !== form.password) {
+          setErrors((e) => ({ ...e, passwordConfirm: "Passwords do not match." }))
+        } else {
+          setErrors((e) => ({ ...e, passwordConfirm: null }))
+        }
+      }
+  
+      setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
+    }
+  
+    const handleOnSubmit = async () => {
+      setIsProcessing(true)
+      setErrors((e) => ({ ...e, form: null }))
+  
+      if (form.passwordConfirm !== form.password) {
+        setErrors((e) => ({ ...e, passwordConfirm: "Passwords do not match." }))
+        setIsProcessing(false)
+        return
+      } else {
+        setErrors((e) => ({ ...e, passwordConfirm: null }))
+      }
+  
+      const { data, error } = await apiClient.signupUser({
+        email: form.email,
+        password: form.password,
+        username: form.username,
+      })
+      if (data) {
+        setUser(data.user)
+        apiClient.setToken(data.token)
+      }
+      if (error) {
+        setErrors((e) => ({ ...e, form: error }))
+      }
+  
+      setIsProcessing(false)
+    }
+
+    return {
+        form,
+        errors,
+        isProcessing,
+        handleOnInputChange,
+        handleOnSubmit
+
+    }
+}
